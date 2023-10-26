9655559304612f0ba976c9f181e84fdd86cd4507
shruggedtom <carlos.matias.acevedo23@gmail.com>
2023-10-25 21:22:19 -0300

    comiteo statics ajustes

--- probando/probando/settings.py
+++ probando/probando/settings.py
@@ -124,5 +124,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'probando/static')]
 
