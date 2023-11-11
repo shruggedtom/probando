5298494e427ffb0eb282f1e1f157611f3802a06f
shruggedtom <carlos.matias.acevedo23@gmail.com>
2023-11-11 18:55:27 -0300

    models

--- probando/house/views.py
+++ probando/house/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from house.models import Product
 
 def index(request):
-    return(render(request,"house/index.html"))
-# Create your views here.
+    products = Product.objects.all()
+    tittle = 'Haciendo cosas!!'
+    return(render(request,"house/index.html",{
+        'tittle': tittle ,
+        'products': products ,
+    }))
+
