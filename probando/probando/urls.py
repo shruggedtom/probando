e98d4370a85e509a29b74377b68f98ca303ed42a
shruggedtom <carlos.matias.acevedo23@gmail.com>
2023-11-21 17:04:34 -0300

    cambio de nombres

--- probando/probando/urls.py
+++ probando/probando/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("primera.urls")),
+    path('', include("crear.urls")),
     path('', include("house.urls")),
     path('', include("house.urls")),
 ]
